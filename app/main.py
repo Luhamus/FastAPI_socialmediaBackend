21a733e3e84d988f23556b2dc79a81cb3dc2e7cc
Rasmus Luha <rasmus.luha@gmail.com>
2022-02-24 03:09:18 +0200

    Added CORS access to *

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from . import models
 from .database import engine, SessionLocal
 from .routers import post, user, auth, vote
@@ -6,6 +8,15 @@ from .config import settings
 
 #models.Base.metadata.create_all(bind=engine) -- was needed for sqlalchemy before alembic
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
